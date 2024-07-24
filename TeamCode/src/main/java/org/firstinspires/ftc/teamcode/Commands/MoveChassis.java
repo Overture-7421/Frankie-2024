ddd1bb6dad50ff8de4182b5f3d686db7123c297f
SantiQ <a01571222@itesm.mx>
2024-07-24 00:35:44 -0600

    First Push

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/MoveChassis.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.Commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.Controllers.FRCTrapezoidProfile;
+import org.firstinspires.ftc.teamcode.Subsystems.Chassis;
+import org.firstinspires.ftc.teamcode.Utils.JoystickHandler;
+
+
+public class MoveChassis extends CommandBase {
+    private final Chassis chassis;
+    private final Gamepad driverGamepad;
+
+    private final FRCTrapezoidProfile leftProfile = new FRCTrapezoidProfile(new FRCTrapezoidProfile.Constraints(0.7, 999));
+    private final FRCTrapezoidProfile rightProfile = new FRCTrapezoidProfile(new FRCTrapezoidProfile.Constraints(1.5,999));
+
+    private FRCTrapezoidProfile.State rightGoal = new FRCTrapezoidProfile.State();
+    private FRCTrapezoidProfile.State leftGoal = new FRCTrapezoidProfile.State();
+
+
+    public MoveChassis(Chassis subsystem, Gamepad driverGamepad) {
+        this.driverGamepad = driverGamepad;
+        chassis = subsystem;
+        addRequirements(subsystem);
+    }
+
+    @Override
+    public void execute(){
+        double right = -driverGamepad.right_stick_x;
+        double left = -driverGamepad.left_stick_y;
+
+        right = JoystickHandler.handleJoystickInput(right);
+        left = JoystickHandler.handleJoystickInput(left);
+
+        rightGoal = rightProfile.calculate(0.05, rightGoal, new FRCTrapezoidProfile.State(right, 0.0));
+        leftGoal = leftProfile.calculate(0.05, leftGoal, new FRCTrapezoidProfile.State(left, 0.0));
+
+        chassis.setSpeed(leftGoal.position, rightGoal.position * 0.5
+        );
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainSystem.java
@@ -0,0 +1,84 @@
+package org.firstinspires.ftc.teamcode;
+/*
+    ______                 __   _         ____        __          __     ______          __
+   / ____/________ _____  / /__(_)__     / __ \____  / /_  ____  / /_   / ____/___  ____/ /__
+  / /_  / ___/ __ `/ __ \/ //_/ / _ \   / /_/ / __ \/ __ \/ __ \/ __/  / /   / __ \/ __  / _ \
+ / __/ / /  / /_/ / / / / ,< / /  __/  / _, _/ /_/ / /_/ / /_/ / /_   / /___/ /_/ / /_/ /  __/
+/_/   /_/   \__,_/_/ /_/_/|_/_/\___/  /_/ |_|\____/_.___/\____/\__/   \____/\____/\__,_/\___/
+
+This is the code to control team Overture 23619's robot "FRANKIE".
+Future iterations may change the overall functionality, though it will be all used for the 2024 FIRST GLOBALS competition.
+All rights reserved. Copyright Overture 23619. Overture holds the right to modify and distribute this code.
+*/
+
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.button.Button;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+
+// Commands Import
+import org.firstinspires.ftc.teamcode.Commands.MoveClaw;
+import org.firstinspires.ftc.teamcode.Commands.MoveChassis;
+
+// Subsystems Import
+import org.firstinspires.ftc.teamcode.Commands.MoveSingleArm;
+import org.firstinspires.ftc.teamcode.Subsystems.Claw;
+import org.firstinspires.ftc.teamcode.Subsystems.Chassis;
+import org.firstinspires.ftc.teamcode.Subsystems.SingleArm;
+
+
+@TeleOp
+public class MainSystem extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        CommandScheduler.getInstance().cancelAll();
+        CommandScheduler.getInstance().reset();
+
+        Chassis chassis         = new Chassis(hardwareMap);     // Create an instance of Chassis
+        SingleArm singleArm     = new SingleArm(hardwareMap);   // Create an instance of SingleArm
+        Claw claw               = new Claw(hardwareMap);        // Create an instance of Claw
+        GamepadEx driverOp      = new GamepadEx(gamepad1);      // Create an instance of DriverGamepad
+
+        // -- CHASSIS MOVEMENT -- //
+        chassis.setDefaultCommand(new MoveChassis(chassis,gamepad1));
+
+        // -- ARM MOVEMENT -- //
+        Button driverDpadDOWN= driverOp.getGamepadButton(GamepadKeys.Button.DPAD_DOWN);
+        driverDpadDOWN.whenPressed(new MoveSingleArm(singleArm, -53));
+
+        Button driverDpadUP= driverOp.getGamepadButton(GamepadKeys.Button.DPAD_UP);
+        driverDpadUP.whenPressed(new MoveSingleArm(singleArm, -10));
+
+        // -- CLAW MOVEMENT -- //
+        Button driverButtonX= driverOp.getGamepadButton(GamepadKeys.Button.X);
+        driverButtonX.whenPressed(new MoveClaw(claw, 0.7));
+
+        Button driverButtonB= driverOp.getGamepadButton(GamepadKeys.Button.B);
+        driverButtonB.whenPressed(new MoveClaw(claw, 0.45));
+
+        waitForStart();
+        chassis.resetPose(new Pose2d(0,0, Rotation2d.fromDegrees(0)));
+
+        while (opModeIsActive()) {
+            CommandScheduler.getInstance().run();
+            Pose2d pose = chassis.getPose();
+
+            // -- ODOMETRY TELEMETRY -- //
+            telemetry.addData("X", pose.getX());
+            telemetry.addData("Y", pose.getY());
+            telemetry.addData("Heading", pose.getRotation().getDegrees());
+            telemetry.addData("RightDistance", chassis.rightDistance());
+            telemetry.addData("LeftDistance", chassis.leftDistance());
+            telemetry.addData("Position", singleArm.getPosition() * 360);
+
+            // -- UPDATE TELEMETRY -- //
+            telemetry.update();
+        }
+    }
+}
